9cdcfbd0dc3278153ab23549f524d13488111129
Leo <leo_bel@hotmail.com>
2022-05-13 18:06:51 -0400

    added images from drupal

--- components/ArticleLayout.tsx
+++ components/ArticleLayout.tsx
@@ -1,9 +1,11 @@
-import { Button, Typography, Divider, Box } from '@mui/material'
+import { Button, Typography, Divider, Box, Container, Paper, Stack } from '@mui/material'
 import { DrupalNode } from 'next-drupal'
 import router from 'next/router'
+import Image from 'next/image'
 import React from 'react'
+import { DrupalArticle } from 'lib/model'
 
-const ArticleLayout = ({ article, baseUrl }: { article: DrupalNode; baseUrl: string }) => {
+const ArticleLayout = ({ article, baseUrl }: { article: DrupalArticle; baseUrl: string }) => {
   return (
     <>
       <Button
@@ -16,6 +18,12 @@ const ArticleLayout = ({ article, baseUrl }: { article: DrupalNode; baseUrl: str
       <Typography variant='h6'>{article.attributes.title}</Typography>
       <Divider></Divider>
       <Typography variant='body1'>{article.attributes.body.summary}</Typography>
+      {article.imageUrl && (
+        <Stack direction='row' justifyContent='center' my={3}>
+          <Image style={{ borderRadius: '.8rem' }} height={400} width={640} src={article.imageUrl} layout='intrinsic' placeholder='empty' />
+        </Stack>
+      )}
+
       <Box dangerouslySetInnerHTML={{ __html: article.attributes.body.processed }}></Box>
     </>
   )
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['dev-rendomstuff00.pantheonsite.io'],
+  },
 }
 
 module.exports = nextConfig
