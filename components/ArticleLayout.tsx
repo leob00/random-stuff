16097361ee399a6ae6f0e23bd7e1f92ea0b98b60
Leo <leo_bel@hotmail.com>
2022-05-15 16:20:11 -0400

    image experiments

--- components/ArticleLayout.tsx
+++ components/ArticleLayout.tsx
@@ -20,8 +20,8 @@ const ArticleLayout = ({ article, baseUrl }: { article: DrupalArticle; baseUrl: 
       <Typography variant='body1'>{article.attributes.body.summary}</Typography>
       {
         article.imageUrl && article.fileMeta && (
-          <Stack direction='row' justifyContent='center' sx={{ height: article.fileMeta.height / 2, width: article.fileMeta.width / 2, my: 2 }}>
-            <Image style={{ borderRadius: '.8rem' }} src={article.imageUrl} layout='intrinsic' objectFit='contain' placeholder='blur' height={article.fileMeta.height} width={article.fileMeta.width} blurDataURL={article.imageUrl} />
+          <Stack direction='row' justifyContent='center' sx={{ my: 2 }}>
+            <Image style={{ borderRadius: '.8rem' }} src={article.imageUrl} placeholder='blur' height={article.fileMeta.height / 2} width={article.fileMeta.width / 2} blurDataURL={article.imageUrl} />
           </Stack>
         )
         // article.fileMeta && <Image style={{ borderRadius: '.8rem', marginTop: 2 }} height={article.fileMeta.height} width={article.fileMeta.width} src={article.imageUrl} placeholder='blur' blurDataURL={article.imageUrl} />}
