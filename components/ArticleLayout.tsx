2d17b6e831c30f69e90fc38eb4f7661889ecee65
Leo <leo_bel@hotmail.com>
2022-05-15 17:20:18 -0400

    added recipes

--- components/ArticleLayout.tsx
+++ components/ArticleLayout.tsx
@@ -1,5 +1,4 @@
 import { Button, Typography, Divider, Box, Container, Paper, Stack } from '@mui/material'
-import { DrupalNode } from 'next-drupal'
 import router from 'next/router'
 import Image from 'next/image'
 import React from 'react'
@@ -18,14 +17,11 @@ const ArticleLayout = ({ article, baseUrl }: { article: DrupalArticle; baseUrl: 
       <Typography variant='h6'>{article.attributes.title}</Typography>
       <Divider></Divider>
       <Typography variant='body1'>{article.attributes.body.summary}</Typography>
-      {
-        article.imageUrl && article.fileMeta && (
-          <Stack direction='row' justifyContent='center' sx={{ my: 2 }}>
-            <Image style={{ borderRadius: '.8rem' }} src={article.imageUrl} placeholder='blur' height={article.fileMeta.height / 2} width={article.fileMeta.width / 2} blurDataURL={article.imageUrl} />
-          </Stack>
-        )
-        // article.fileMeta && <Image style={{ borderRadius: '.8rem', marginTop: 2 }} height={article.fileMeta.height} width={article.fileMeta.width} src={article.imageUrl} placeholder='blur' blurDataURL={article.imageUrl} />}
-      }
+      {article.imageUrl && article.fileMeta && (
+        <Stack direction='row' justifyContent='center' sx={{ my: 2 }}>
+          <Image style={{ borderRadius: '.8rem' }} src={article.imageUrl} placeholder='blur' height={article.fileMeta.height / 2} width={article.fileMeta.width / 2} blurDataURL={article.imageUrl} />
+        </Stack>
+      )}
       <Box dangerouslySetInnerHTML={{ __html: article.attributes.body.processed }}></Box>
     </>
   )
