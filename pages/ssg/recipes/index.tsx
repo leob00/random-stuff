08e19aa3e4348400f0fef720c9b5d1f84500466e
Leo <leo_bel@hotmail.com>
2022-05-18 11:41:58 -0400

    added featured article

--- pages/ssg/recipes/index.tsx
+++ pages/ssg/recipes/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
-import { Container, Typography, Button } from '@mui/material'
-import { getRecipes } from 'lib/drupalApi'
+import { Container, Typography, Button, Divider } from '@mui/material'
+import { getDrupalArticle, getRecipes } from 'lib/drupalApi'
 import { DrupalNode } from 'next-drupal'
 import router from 'next/router'
 import useSWR, { SWRConfig } from 'swr'
 import axios from 'axios'
 import ArticleTableLayout from 'components/ArticleTableLayout'
+import { DrupalArticle } from 'lib/model'
 
 const cmsRefreshIntervalSeconds = 3600
 const cmsRefreshIntervalMs = cmsRefreshIntervalSeconds * 1000
@@ -17,7 +18,10 @@ const fetcherFn = async (url: string) => {
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  var articles = await getRecipes()
+  let articles = await getRecipes()
+  let random = articles[Math.floor(Math.random() * (articles.length - 1))]
+  //console.log('featured: ', JSON.stringify(featured))
+  let featured = await getDrupalArticle(random.id)
 
   return {
     props: {
@@ -25,12 +29,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
       fallback: {
         '/api/recipes': articles,
       },
+      featured,
     },
     revalidate: cmsRefreshIntervalSeconds,
   }
 }
 
-const Articles = ({ fallbackData }: { fallbackData: DrupalNode[] }) => {
+const Articles = ({ fallbackData, featuredArticle }: { fallbackData: DrupalNode[]; featuredArticle: DrupalArticle }) => {
   const { data, error } = useSWR(['/api/recipes'], (url: string) => fetcherFn(url), {
     fallbackData: fallbackData,
     refreshInterval: cmsRefreshIntervalMs,
@@ -42,10 +47,10 @@ const Articles = ({ fallbackData }: { fallbackData: DrupalNode[] }) => {
   if (!articles) {
     return <Container>loading...</Container>
   }
-  return <ArticleTableLayout articles={articles} baseUrl='/ssg/recipes/' />
+  return <ArticleTableLayout articles={articles} baseUrl='/ssg/recipes/' featuredArticle={featuredArticle} />
 }
 
-const Recipes: NextPage<{ articles: DrupalNode[]; fallback: any }> = ({ articles, fallback }) => {
+const Recipes: NextPage<{ articles: DrupalNode[]; fallback: any; featured: DrupalArticle }> = ({ articles, fallback, featured }) => {
   return (
     <>
       <Button
@@ -56,8 +61,9 @@ const Recipes: NextPage<{ articles: DrupalNode[]; fallback: any }> = ({ articles
         &#8592; back
       </Button>
       <Typography variant='h6'>Recipes</Typography>
+      <Divider />
       <SWRConfig value={{ fallback }}>
-        <Articles fallbackData={articles} />
+        <Articles fallbackData={articles} featuredArticle={featured} />
       </SWRConfig>
     </>
   )
