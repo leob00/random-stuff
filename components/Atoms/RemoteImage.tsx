5351d323da0cb849ee7730dc3fa0130ddaa9a42c
Leo <leo_bel@hotmail.com>
2022-06-23 09:44:30 -0400

    error handling improvements

--- components/Atoms/RemoteImage.tsx
+++ components/Atoms/RemoteImage.tsx
@@ -5,10 +5,11 @@ import { Stack, Box } from '@mui/material'
 const RemoteImage = ({ url, title, width = 350, height = 400 }: { url: string; title: string; width?: number; height?: number }) => {
   const hght = height ? `${height}px` : '400px'
   const wdth = width ? `${width}px` : '350px'
+
   return (
     <>
-      <Box sx={{ position: 'relative', height: hght, width: wdth, padding: '5px', my: 1 }} className=''>
-        <Image style={{}} src={url} alt={title} placeholder='blur' layout='fill' objectFit='cover' loading='lazy' blurDataURL={url} />
+      <Box sx={{ position: 'relative', height: hght, width: wdth, padding: '5px', my: 1, borderRadius: '10px' }} className='blue-gradient'>
+        <Image style={{ borderRadius: '10px' }} src={url} alt={title} placeholder='blur' layout='fill' objectFit='cover' loading='lazy' blurDataURL={url} />
       </Box>
     </>
   )
