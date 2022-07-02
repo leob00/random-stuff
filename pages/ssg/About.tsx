4b41d61b9a0295c24e8984d3245a01b70cb8af0d
Leo <leo_bel@hotmail.com>
2022-07-02 14:41:20 -0400

    header spacing fixes

--- pages/ssg/About.tsx
+++ pages/ssg/About.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
-import { Container, Paper, Box, Typography, Grid, Button, Divider } from '@mui/material'
+import { Container, Paper, Box, Typography } from '@mui/material'
 import Image from 'next/image'
-import router from 'next/router'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   //var data = await getYieldCurveData()
@@ -16,61 +15,24 @@ const About: NextPage<{ data: any }> = ({ data }) => {
   return (
     <Container sx={{ minHeight: '640px' }}>
       <Typography variant='h5'>About Us</Typography>
-
-      <Paper sx={{ paddingTop: '10px' }}>
-        <Box sx={{ align: 'center', mx: 2 }}>
-          <Typography sx={{ textAlign: 'center' }}>
-            <Image priority src='/images/logo-with-text.png' width={340} height={250} alt='random things' style={{ borderRadius: '.6rem' }} />
-          </Typography>
-          <Typography variant='body2' align='center' gutterBottom>
-            This site is dedicated to random foolishness and inconsequential musings. If you made it this far, it means there is no turning back any time soon. Sit back and relax and watch the grass grow, paint dry, or whatever else you find
-            interesting.
-            <br />
-            Maybe consider embracing the Oxford comma...
-          </Typography>
-          <Box sx={{ textAlign: 'center' }}>
-            <Grid container alignItems='center'>
-              <Grid item>
-                <Button
-                  variant='text'
-                  onClick={() => {
-                    router.push('/')
-                  }}>
-                  home
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button
-                  variant='text'
-                  onClick={() => {
-                    router.push('/csr/RandomCat')
-                  }}>
-                  random cats
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button
-                  variant='text'
-                  onClick={() => {
-                    router.push('/csr/RandomDog')
-                  }}>
-                  random dogs
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button
-                  variant='text'
-                  onClick={() => {
-                    router.push('/ssg/recipes')
-                  }}>
-                  recipes
-                </Button>
-              </Grid>
-            </Grid>
+      <Box sx={{ my: 2, paddingBottom: 3 }}>
+        <Paper>
+          <Box sx={{ align: 'center', my: 1 }}>
+            <Typography sx={{ textAlign: 'center' }}>
+              <Image priority src='/images/logo-with-text.png' width={340} height={250} alt='random things' style={{ borderRadius: '.6rem' }} />
+            </Typography>
           </Box>
-          <Typography align='center' sx={{ padding: '10px' }}></Typography>
-        </Box>
-      </Paper>
+          <Box sx={{ align: 'center', my: 2 }}>
+            <Typography variant='body2' align='center' gutterBottom>
+              This site is dedicated to random foolishness and inconsequential musings. If you made it this far, it means there is no turning back any time soon. Sit back and relax and watch the grass grow, paint dry, or whatever else you find
+              interesting.
+              <br />
+              Maybe consider embracing the Oxford comma...
+            </Typography>
+            <Typography sx={{ padding: 2 }}></Typography>
+          </Box>
+        </Paper>
+      </Box>
     </Container>
   )
 }
