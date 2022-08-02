c35e6e4c0e5a7b8713a04d01e057552b00fff442
Leo <leo_bel@hotmail.com>
2022-08-02 11:37:50 -0400

    randomized animal layout

--- components/Organizms/ArticlesLayout.tsx
+++ components/Organizms/ArticlesLayout.tsx
@@ -3,20 +3,21 @@ import CenteredTitle from 'components/Atoms/Containers/CenteredTitle'
 import Pager from 'components/Atoms/Pager'
 import RemoteImage from 'components/Atoms/RemoteImage'
 import { BasicArticle } from 'lib/model'
-import { PagedCollection, pageItems } from 'lib/util/collections'
-import { findLast } from 'lodash'
+import { pageItems } from 'lib/util/collections'
+import { cloneDeep, findLast, shuffle } from 'lodash'
 import router from 'next/router'
 import React, { useState } from 'react'
 
 const ArticlesLayout = ({ articles }: { articles: BasicArticle[] }) => {
+  const shuffled = shuffle(cloneDeep(articles))
   const itemsPerPage = 1
-  const paged = pageItems(articles, itemsPerPage) as PagedCollection
+  const paged = pageItems(shuffled, itemsPerPage)
   const [currentPageIndex, setCurrentPageIndex] = useState(1)
   const [displayedItems, setDisplayedItems] = useState(paged.pages[0].items)
 
   const handlePaged = (pageNum: number) => {
     setCurrentPageIndex(pageNum)
-    let page = findLast(paged.pages, function (p) {
+    let page = findLast(paged.pages, (p) => {
       return p.index === pageNum
     })
     if (page) {
@@ -37,14 +38,9 @@ const ArticlesLayout = ({ articles }: { articles: BasicArticle[] }) => {
               }}>
               &#8592; back
             </Button>
-            <CenteredTitle title={articles[0].title} />
+            <CenteredTitle title={displayedItems[0].title} />
             <Divider />
           </Box>
-          {/* {articles.map((item) => (
-            <Stack direction='row' justifyContent='center' my={2} key={item.imagePath}>
-              <RemoteImage url={item.imagePath} title={item.title} />
-            </Stack>
-          ))} */}
           <Stack direction='row' justifyContent='center' my={2} key={articles[0].imagePath}>
             <RemoteImage url={displayedItems[0].imagePath} title={displayedItems[0].title} onLoaded={handleImageLoaded} />
           </Stack>
