bca7c2b0e78a4b353d7ea428cfbaef292076ceb2
Leo <leo_bel@hotmail.com>
2022-08-13 16:25:25 -0400

    experimenting with link preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetStaticProps, NextPage } from 'next'
 import HomeMenu from 'components/Organizms/HomeMenu'
+import Header from 'next/head'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   //var data = await getYieldCurveData()
@@ -10,6 +11,12 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const Home: NextPage = () => {
   return (
     <>
+      <Header>
+        <title>Random Stuff - Home</title>
+        <meta property='og:title' content='Random Stuff - Home' key='homeTitle' />
+        <meta property='og:description' content='Random Stuff: this site is dedicated to random foolishness and inconsequential musings.' key='homeDescription' />
+        <meta property='og:image' content='/images/logo-with-text-blue.png' key='homeLogo' />
+      </Header>
       <HomeMenu />
     </>
   )
