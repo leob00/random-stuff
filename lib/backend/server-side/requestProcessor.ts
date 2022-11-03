77e3a398031c76539bad79f57ff98dda6cbb37e7
Leo <leo_bel@hotmail.com>
2022-11-03 09:00:12 -0400

    request auth - in progress

--- lib/backend/server-side/requestProcessor.ts
+++ lib/backend/server-side/requestProcessor.ts
@@ -2,15 +2,13 @@ import { isBrowser } from 'lib/util/system'
 import { findLast } from 'lodash'
 import { NextApiRequest } from 'next'
 
-export async function authorizeRequest(req: NextApiRequest) {
-  const isClientSide = isBrowser()
-
-  const authCookie = findLast(Object.keys(req.cookies), (e) => {
-    return e.includes('CognitoIdentityServiceProvider')
-  })
-  console.log(`authorizing request: ${isClientSide ? 'client' : 'server'}`)
-  if (authCookie) {
-    return true
+export async function authorizeGetRequest(req: NextApiRequest) {
+  const providedToken = req.query['token'] as string | undefined
+  if (!providedToken) {
+    return false
   }
-  return false
+  if (process.env.NEXT_PUBLIC_API_TOKEN !== providedToken) {
+    return false
+  }
+  return true
 }
--- pages/api/randomStuff.ts
+++ pages/api/randomStuff.ts
@@ -1,23 +1,15 @@
 import { getRandomStuff } from 'lib/backend/api/aws/apiGateway'
+import { authorizeGetRequest } from 'lib/backend/server-side/requestProcessor'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  let id = req.query['id'] as string
-  let providedToken = req.query['token'] as string | undefined
-  if (!providedToken) {
+  if (!authorizeGetRequest(req)) {
     res.status(403).send('unuathorized call')
     console.log('unauthorized call')
     return
   }
-  // const token = myEncrypt(`${id}${getRandomLoaderText()}`, 'what a wonderful world')
-  if (process.env.NEXT_PUBLIC_API_TOKEN !== providedToken) {
-    res.status(403).send('unuathorized call')
-    console.log('unauthorized call')
-    return
-  }
-  console.log('public token:   ', process.env.NEXT_PUBLIC_API_TOKEN)
-  //console.log('provided token: ', providedToken)
 
+  const id = req.query['id'] as string
   var result = await getRandomStuff(id)
   res.status(200).json(result)
 }
