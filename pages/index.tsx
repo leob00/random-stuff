823a5555f87d2e38a66e079fa4c1a9192800dbe9
Leo <leo_bel@hotmail.com>
2022-11-17 18:37:19 -0500

    seo work

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <title>Random Stuff - Home</title>
         <meta property='og:title' content='Random Stuff: this site is dedicated to random foolishness and inconsequential musings.' key='homeTitle' />
         <meta property='og:description' content='Find recipes, create notes, and view random pictures. Happy browsing!' key='homeDescription' />
-        <meta property='og:image' content='https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small.png' key='homeLogo' />
+        <meta property='og:image' content='https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small1200X630.png' key='homeLogo' />
       </Header>
       <HomeMenu />
     </>
