b483aea982e5bc3d7a5a084793c745ce7e6cbd27
Leo <leo_bel@hotmail.com>
2023-04-03 14:42:18 -0400

    Seo refactoring

--- components/Organizms/Seo.tsx
+++ components/Organizms/Seo.tsx
@@ -5,16 +5,22 @@ const Seo = ({ pageTitle }: { pageTitle: string }) => {
   return (
     <Header>
       <title>{`Random Stuff - ${pageTitle}`}</title>
-      <meta name='twitter:card' content='summary_large_image'></meta>
-      <meta property='og:title' content='Random Stuff: this site is dedicated to random foolishness and inconsequential musings.' key='homeTitle' />
+      {/* Facebook */}
+      <meta property='og:title' content='Random Stuff: this site is dedicated to random foolishness and inconsequential musings.' />
+      <meta property='og:description' content='Find recipes, create notes, and view random pictures. Happy browsing!' />
+      <meta property='og:image' content='https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small-social.png' />
+      {/* Google */}
+      <meta itemProp='name' content={`Random Stuff - ${pageTitle}`} />
+      <meta itemProp='description' content={`Find recipes, create notes, and view random pictures. Happy browsing!`} />
+      <meta itemProp='image' content={`https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small-social.png`} />
+      {/* Twitter */}
       <meta
         property='twitter:title'
         content='Random Stuff: this site is dedicated to random foolishness and inconsequential musings.'
         key='twitter-homeTitle'
       />
-      <meta property='og:description' content='Find recipes, create notes, and view random pictures. Happy browsing!' key='homeDescription' />
-      <meta property='twitter:description' content='Find recipes, create notes, and view random pictures. Happy browsing!' key='twitter-homeDescription' />
-      <meta property='og:image' content='https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small-social.png' key='homeLogo' />
+      <meta property='twitter:description' content='Find recipes, create notes, and view random pictures. Happy browsing!' />
+      <meta name='twitter:card' content='summary_large_image'></meta>
     </Header>
   )
 }
