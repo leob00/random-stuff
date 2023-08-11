3b06fa82b7727932022f959ab23186de17238f61
Leo <leo_bel@hotmail.com>
2023-08-11 14:07:42 -0400

    dark mode implemented!

--- components/Atoms/Charts/BasicLineChart.tsx
+++ components/Atoms/Charts/BasicLineChart.tsx
@@ -1,15 +1,15 @@
-import { Box, useMediaQuery } from '@mui/material'
+import { Box, useMediaQuery, useTheme } from '@mui/material'
 import { ApexOptions } from 'apexcharts'
 import WarmupBox from 'components/Atoms/WarmupBox'
 import { XyValues } from 'components/Molecules/Charts/apex/models/chartModes'
 import { getOptions } from 'components/Organizms/stocks/lineChartOptions'
-import theme from 'components/themes/mainTheme'
 import dynamic from 'next/dynamic'
 import React from 'react'
 
 const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 const BasicLineChart = ({ xyValues, rawData }: { xyValues: XyValues; rawData: any[] }) => {
+  const theme = useTheme()
   const isXSmall = useMediaQuery(theme.breakpoints.down('md'))
   const isLarge = useMediaQuery(theme.breakpoints.up('lg'))
   let chartHeight = 580
@@ -21,7 +21,7 @@ const BasicLineChart = ({ xyValues, rawData }: { xyValues: XyValues; rawData: an
   const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
-    setChartOptions(getOptions(xyValues, rawData, isXSmall))
+    setChartOptions(getOptions(xyValues, rawData, isXSmall, theme.palette.mode))
     setIsLoading(false)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
