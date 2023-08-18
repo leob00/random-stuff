802dd0326e834e4622603e2b57ef43d33320f0ff
Leo <leo_bel@hotmail.com>
2023-08-18 13:37:36 -0400

    Earnings calendar - beta

--- components/Organizms/stocks/EarningsCalendarLayout.tsx
+++ components/Organizms/stocks/EarningsCalendarLayout.tsx
@@ -1,9 +1,10 @@
-import { Box, Link, Paper, Stack, Typography } from '@mui/material'
+import { Box, Link, Paper, Stack, Typography, useTheme } from '@mui/material'
 import LinkButton2 from 'components/Atoms/Buttons/LinkButton2'
 import BackdropLoader from 'components/Atoms/Loaders/BackdropLoader'
 import LargeGridSkeleton from 'components/Atoms/Skeletons/LargeGridSkeleton'
 import NoDataFound from 'components/Atoms/Text/NoDataFound'
 import ListHeader from 'components/Molecules/Lists/ListHeader'
+import { CasinoBlue, CasinoBlueTransparent, DarkBlue, DarkBlueTransparent } from 'components/themes/mainTheme'
 import dayjs from 'dayjs'
 import { apiConnection } from 'lib/backend/api/config'
 import { get } from 'lib/backend/api/fetchFunctions'
@@ -12,6 +13,7 @@ import { orderBy, uniq } from 'lodash'
 import numeral from 'numeral'
 import React from 'react'
 import useSWR, { Fetcher } from 'swr'
+import StockEarningsCalendarDetails from './StockEarningsCalendarDetails'
 import StockEarningsTable from './StockEarningsTable'
 
 const config = apiConnection().qln
@@ -20,13 +22,40 @@ const fetcher: Fetcher<QlnApiResponse> = (url: string) => get(url)
 
 const EarningsCalendarLayout = () => {
   const [selectedDate, setSelectedDate] = React.useState<string | null>(null)
-
+  const [scrollIntoView, setScrollIntoView] = React.useState(false)
+  const [filteredResults, setFilteredResults] = React.useState<StockEarning[]>([])
   const { data, isLoading, isValidating } = useSWR(apiUrl, fetcher)
+  const scrollTarget = React.useRef<HTMLSpanElement | null>(null)
+  const theme = useTheme()
+
+  const filterResult = (dt: string) => {
+    //const res = [...items]
+    return orderBy(
+      (data!.Body as StockEarning[]).filter((m) => m.ReportDate === dt),
+      ['StockQuote.Company'],
+      ['asc'],
+    )
+  }
+  const handleDateClicked = (dt: string) => {
+    setScrollIntoView(true)
+    setSelectedDate(dt)
+    setFilteredResults(filterResult(dt))
+  }
+
+  React.useEffect(() => {
+    if (scrollIntoView) {
+      if (scrollTarget.current) {
+        scrollTarget.current.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+    setScrollIntoView(false)
+  }, [scrollIntoView])
 
   const RenderDisplay = (apiResult: QlnApiResponse) => {
     const result = apiResult.Body as StockEarning[]
     const uniqueDates = orderBy(uniq(result.map((m) => m.ReportDate)))
     const datesMap = new Map<string, StockEarning[]>()
+
     uniqueDates.forEach((item) => {
       datesMap.set(
         item!,
@@ -39,15 +68,21 @@ const EarningsCalendarLayout = () => {
         <Box display={'flex'} gap={1} alignItems={'center'} flexWrap={'wrap'} justifyContent='center'>
           {Array.from(datesMap.entries()).map(([key, values]) => (
             <Box key={key}>
-              {/* <ListHeader text={`${dayjs(key).format('MM/DD/YYYY')}`} item={key} onClicked={() => {}} /> */}
               <Box py={1}>
                 <Paper
                   component={Stack}
-                  sx={{ minHeight: { xs: 180, sm: 160 }, p: 2, width: { xs: 150, sm: 240 }, direction: 'column', justifyContent: 'center' }}
+                  sx={{
+                    minHeight: { xs: 180, sm: 160 },
+                    p: 2,
+                    width: { xs: 150, sm: 240 },
+                    direction: 'column',
+                    justifyContent: 'center',
+                    backgroundColor: key === selectedDate ? (theme.palette.mode === 'dark' ? CasinoBlueTransparent : 'whitesmoke') : 'unset',
+                  }}
                 >
                   <Box textAlign={'center'}>
                     <Typography>{`${dayjs(key).format('MM/DD/YYYY')}`}</Typography>
-                    <LinkButton2 onClick={() => setSelectedDate(key)}>{`${numeral(values.length).format('###,###')} earnings`}</LinkButton2>
+                    <LinkButton2 onClick={() => handleDateClicked(key)}>{`${numeral(values.length).format('###,###')} earnings`}</LinkButton2>
                   </Box>
                 </Paper>
               </Box>
@@ -56,14 +91,8 @@ const EarningsCalendarLayout = () => {
         </Box>
         {selectedDate && data && (
           <Box pt={1}>
-            <StockEarningsTable
-              data={orderBy(
-                (data.Body as StockEarning[]).filter((m) => m.ReportDate === selectedDate),
-                ['StockQuote.Company'],
-                ['asc'],
-              )}
-              showCompany
-            />
+            <Typography ref={scrollTarget} sx={{ position: 'absolute', mt: -12 }}></Typography>
+            <StockEarningsCalendarDetails data={filteredResults} />
           </Box>
         )}
       </Box>
@@ -74,7 +103,6 @@ const EarningsCalendarLayout = () => {
     <Box py={2}>
       {isValidating && <BackdropLoader />}
       {isLoading && <LargeGridSkeleton />}
-
       {!isLoading && data && data.Body.length === 0 && <NoDataFound />}
       {data && RenderDisplay(data)}
     </Box>
---
+++ components/Organizms/stocks/StockEarningsCalendarDetails.tsx
@@ -0,0 +1,115 @@
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import CenterStack from 'components/Atoms/CenterStack'
+import { StockEarning } from 'lib/backend/api/qln/qlnApi'
+import numeral from 'numeral'
+import React from 'react'
+import { getPositiveNegativeColor } from './StockListItem'
+import { Box, Typography } from '@mui/material'
+import { useTheme } from '@mui/material'
+import { getPagedArray } from 'lib/util/collections'
+import Pager from 'components/Atoms/Pager'
+import SearchWithinList from 'components/Atoms/Inputs/SearchWithinList'
+
+const StockEarningsCalendarDetails = ({ data }: { data: StockEarning[] }) => {
+  const theme = useTheme()
+  const pageSize = 10
+  const [searchWithinList, setSearchWithinList] = React.useState('')
+
+  const filterList = () => {
+    if (searchWithinList.length === 0) {
+      return [...data]
+    }
+    return data.filter(
+      (f) => f.Symbol.toLowerCase().startsWith(searchWithinList.toLowerCase()) || f.StockQuote?.Company.toLowerCase().includes(searchWithinList.toLowerCase()),
+    )
+  }
+  const pages = getPagedArray(filterList(), pageSize)
+
+  //const [pages, setPages] = React.useState(pagedItems)
+
+  const [currentPageIndex, setCurrentPageIndex] = React.useState(1)
+  const handlePaged = (pageNum: number) => {
+    setCurrentPageIndex(pageNum)
+  }
+  const handleSearched = (text: string) => {
+    setSearchWithinList(text)
+
+    // const filtered = filterList(data)
+    // const newPages = getPagedArray(filtered, pageSize)
+    // setPages(newPages)
+  }
+
+  React.useEffect(() => {
+    //setPages(getPagedArray(filterList(data), pageSize))
+  }, [searchWithinList])
+
+  return (
+    <>
+      <Box pl={1}>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell colSpan={4}>
+                  <SearchWithinList onChanged={handleSearched} />
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableHead>
+              <TableRow>
+                <TableCell>Company</TableCell>
+                <TableCell>Actual</TableCell>
+                <TableCell>Estimate</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {pages.length > 0 &&
+                pages[currentPageIndex - 1].items.map((item, index) => (
+                  <TableRow key={item.Symbol}>
+                    <TableCell>
+                      <Typography>{`${item.StockQuote?.Company} (${item.StockQuote?.Symbol})`}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography color={getPositiveNegativeColor(item.ActualEarnings, theme.palette.mode)}>
+                        {`${item.ActualEarnings ? numeral(item.ActualEarnings).format('0.00') : ''}`}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography color={getPositiveNegativeColor(item.EstimatedEarnings, theme.palette.mode)}>
+                        {`${item.EstimatedEarnings ? numeral(item.EstimatedEarnings).format('0.00') : ''}`}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {pages.length > 0 && (
+          <Box pt={4}>
+            <Pager
+              pageCount={pages.length}
+              itemCount={pages[currentPageIndex - 1].items.length}
+              itemsPerPage={pageSize}
+              onPaged={(pageNum: number) => handlePaged(pageNum)}
+              defaultPageIndex={currentPageIndex}
+              totalItemCount={data.length}
+            ></Pager>
+          </Box>
+        )}
+        {data.length === 0 && (
+          <CenterStack sx={{ py: 4 }}>
+            <Typography variant='body2'>No data found.</Typography>
+          </CenterStack>
+        )}
+      </Box>
+    </>
+  )
+}
+
+export default StockEarningsCalendarDetails
