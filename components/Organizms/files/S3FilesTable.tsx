6dfcc9165260df304b190c384b8ca64e42ab2947
Leo <leo_bel@hotmail.com>
2023-10-08 16:34:15 -0400

    file view

--- components/Organizms/files/S3FilesTable.tsx
+++ components/Organizms/files/S3FilesTable.tsx
@@ -1,6 +1,13 @@
-import { Box, Typography } from '@mui/material'
+import { Button } from '@aws-amplify/ui-react'
+import { Box, Link, Stack, Typography } from '@mui/material'
+import LinkButton2 from 'components/Atoms/Buttons/LinkButton2'
+import PrimaryButton from 'components/Atoms/Buttons/PrimaryButton'
+import CenterStack from 'components/Atoms/CenterStack'
 import ConfirmDeleteDialog from 'components/Atoms/Dialogs/ConfirmDeleteDialog'
+import FormDialog from 'components/Atoms/Dialogs/FormDialog'
+import BackdropLoader from 'components/Atoms/Loaders/BackdropLoader'
 import NoDataFound from 'components/Atoms/Text/NoDataFound'
+import WarmupBox from 'components/Atoms/WarmupBox'
 import ListItemContainer from 'components/Molecules/Lists/ListItemContainer'
 import { S3Object } from 'lib/backend/api/aws/apiGateway'
 import { post, postDelete } from 'lib/backend/api/fetchFunctions'
@@ -21,14 +28,19 @@ const S3FilesTable = ({
 }) => {
   const [itemToDelete, setItemToDelete] = React.useState<S3Object | null>(null)
   const [signedUrl, setSignedUrl] = React.useState<string | null>(null)
+  const [selectedItem, setSelectedItem] = React.useState<S3Object | null>(null)
+  const [isLoading, setIsLoading] = React.useState(false)
   const signedUrlRef = React.useRef<HTMLAnchorElement | null>(null)
   const handleView = async (item: S3Object) => {
+    setIsLoading(true)
     const params = { bucket: item.bucket, prefix: item.prefix, filename: item.filename, expiration: 60 }
     const url = JSON.parse(await post(`/api/s3`, params)) as string
     setSignedUrl(url)
-    if (signedUrlRef.current) {
-      signedUrlRef.current.click()
-    }
+    setSelectedItem(item)
+    setIsLoading(false)
+    // if (signedUrlRef.current) {
+    //   signedUrlRef.current.click()
+    // }
     // const a = document.createElement('a')
     // a.setAttribute('href', url)
     // a.setAttribute('target', '_blank')
@@ -54,6 +66,7 @@ const S3FilesTable = ({
 
   return (
     <>
+      {isLoading && <WarmupBox text='generating a secure link...' />}
       {data.map((item) => (
         <Box key={item.filename}>
           <Box py={1}>
@@ -83,9 +96,27 @@ const S3FilesTable = ({
         />
       )}
       {signedUrl && (
-        <a style={{ display: 'none' }} rel='noreferrer' ref={signedUrlRef} href={signedUrl} target={'_blank'}>
-          link
-        </a>
+        <FormDialog
+          title='secure file link'
+          show={true}
+          onCancel={() => {
+            setSelectedItem(null)
+            setSignedUrl(null)
+          }}
+        >
+          <>
+            <Stack>
+              <CenterStack>
+                <Typography textAlign={'center'}>{selectedItem?.filename}</Typography>
+              </CenterStack>
+            </Stack>
+            <CenterStack sx={{ pt: 2 }}>
+              <Link rel='noreferrer' ref={signedUrlRef} href={signedUrl} target={'_blank'}>
+                <PrimaryButton text={'view file'} onClick={() => setSignedUrl(null)}></PrimaryButton>
+              </Link>
+            </CenterStack>
+          </>
+        </FormDialog>
       )}
     </>
   )
