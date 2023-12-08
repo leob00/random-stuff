71385187ca31689f518c76698f646690d091f5da
Leo <leo_bel@hotmail.com>
2023-12-08 09:20:51 -0500

    improved cron error handling

--- app/api/cron/stockAlerts/route.ts
+++ app/api/cron/stockAlerts/route.ts
@@ -5,14 +5,18 @@ import { buildStockAlertsForAllUsers } from 'lib/backend/alerts/stockAlertBulder
 export async function GET(req: NextRequest) {
   const authHeader = req.headers.get('authorization')
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    console.error('/api/cron/stockAlerts - unauthorized')
     return new Response('Unauthorized', {
       status: 401,
     })
   }
-
-  const emailMessages = await buildStockAlertsForAllUsers()
-  for (const email of emailMessages) {
-    await sendEmail(email)
+  try {
+    const emailMessages = await buildStockAlertsForAllUsers()
+    for (const email of emailMessages) {
+      await sendEmail(email)
+    }
+  } catch (err) {
+    console.error(`api/cron/stockAlerts error: ${err}`)
   }
 
   return NextResponse.json({ message: 'cron job completed' })
