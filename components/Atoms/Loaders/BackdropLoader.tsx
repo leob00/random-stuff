2b88dad8584f520b064fb2655e4d0b87d8c82a4e
Leo <leo_bel@hotmail.com>
2023-12-14 20:39:40 -0500

    loader changes

--- components/Atoms/Loaders/BackdropLoader.tsx
+++ components/Atoms/Loaders/BackdropLoader.tsx
@@ -6,9 +6,11 @@ const BackdropLoader = () => {
   return (
     <Box>
       <Backdrop sx={{ backgroundColor: VeryLightTransparent, zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
-        <img src={'/images/loaders/blue-ring-expanded.svg'} alt='loader' />
-        <img src={'/images/loaders/blue-ring.svg'} alt='loader' />
-        <img src={'/images/loaders/blue-ring-expanded.svg'} alt='loader' />
+        <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+          <img src={'/images/loaders/blue-ring-expanded.svg'} alt='loader' />
+          <img src={'/images/loaders/blue-ring.svg'} alt='loader' />
+          <img src={'/images/loaders/blue-ring-expanded.svg'} alt='loader' />
+        </Box>
         {/* <img src={'/images/loaders/dots.svg'} alt='loader' /> */}
       </Backdrop>
     </Box>
