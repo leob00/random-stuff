a80b9caf5378a50821406d6dc60541ade0f6fd34
Leo <leo_bel@hotmail.com>
2023-12-19 12:58:38 -0500

    ui effects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { GetStaticProps, NextPage } from 'next'
 import HomeMenu from 'components/Organizms/HomeMenu'
 import Seo from 'components/Organizms/Seo'
-import BackdropLoader from 'components/Atoms/Loaders/BackdropLoader'
 
 const Home = () => {
   return (
     <>
-      <BackdropLoader />
       <Seo pageTitle='Home' />
       <HomeMenu />
     </>
