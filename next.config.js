4d21b2501b84b4bfe09ff05b151330c4d45b45c6
Leo <leo_bel@hotmail.com>
2023-12-26 14:04:41 -0500

    maintenance

--- next.config.js
+++ next.config.js
@@ -8,13 +8,40 @@ const nextConfig = {
     },
   },
   images: {
-    domains: [
-      'dev-rendomstuff00.pantheonsite.io',
-      'images.ctfassets.net',
-      'cdn2.thecatapi.com',
-      'images.dog.ceo',
-      'imgs.xkcd.com',
-      'cso88ynaac.execute-api.us-east-1.amazonaws.com',
+    // domains: [
+    //   'dev-rendomstuff00.pantheonsite.io',
+    //   'images.ctfassets.net',
+    //   'cdn2.thecatapi.com',
+    //   'images.dog.ceo',
+    //   'imgs.xkcd.com',
+    //   'cso88ynaac.execute-api.us-east-1.amazonaws.com',
+    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cso88ynaac.execute-api.us-east-1.amazonaws.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.dog.ceo',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn2.thecatapi.com',
+        pathname: '**',
+      },
     ],
     //deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     //imageSizes: [8, 16, 32, 48, 64, 96, 128, 256, 384],
