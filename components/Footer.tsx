85b13847953c9f3e48ef1a301d45d6ff8cff12f8
Leo <leo_bel@hotmail.com>
2023-12-27 10:30:58 -0500

    maintenance

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,34 +1,16 @@
-import * as React from 'react'
 import { Box, Container, Divider, Stack, Typography } from '@mui/material'
 import dayjs from 'dayjs'
-import HorizontalDivider from './Atoms/Dividers/HorizontalDivider'
+import HorizontalDivider from 'components/Atoms/Dividers/HorizontalDivider'
 
 const Footer = () => {
-  const [isLoading, setIsLoading] = React.useState(true)
-
   return (
     <Container>
       <Container>
         <HorizontalDivider />
-
         <Stack direction='row' display={'flex'} spacing={2} divider={<Divider orientation='vertical' flexItem />} py={4} alignItems={'center'}>
           <Box>
             <Typography variant='caption'>©{dayjs().format('YYYY')} Random Stuff</Typography>
           </Box>
-          {/* <Box>
-            {isLoading ? (
-              <RollingLinearProgress height={20} />
-            ) : (
-              <Box>
-                <Stack alignItems={'center'} flexDirection={'row'}>
-                  <Circle color={'secondary'} fontSize='small' />
-                  <Typography variant='caption' pl={1}>
-                    online
-                  </Typography>
-                </Stack>
-              </Box>
-            )}
-          </Box> */}
         </Stack>
       </Container>
     </Container>
