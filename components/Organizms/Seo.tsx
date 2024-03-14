eec91ab7a2e1233b8c6f26a141fa2662c6603123
Leo <leo_bel@hotmail.com>
2024-03-14 08:51:43 -0400

    SEO experiments

--- components/Organizms/Seo.tsx
+++ components/Organizms/Seo.tsx
@@ -5,8 +5,11 @@ const Seo = ({ pageTitle }: { pageTitle: string }) => {
   return (
     <Header>
       <title>{`Random Stuff - ${pageTitle}`}</title>
+      <meta name='description' content='Find recipes, create goals and notes, track stocks and view random pictures. Happy browsing!' />
       {/* Facebook */}
+      <meta property='og:url' content='https://random-stuff-seven.vercel.app' />
       <meta property='og:title' content={`Random Stuff - ${pageTitle}`} />
+      <meta property='og:type' content='website' />
       <meta property='og:description' content='Find recipes, create goals and notes, track stocks and view random pictures. Happy browsing!' />
       <meta property='og:image' content='https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small-social.png' />
       {/* Google */}
@@ -14,9 +17,12 @@ const Seo = ({ pageTitle }: { pageTitle: string }) => {
       <meta itemProp='description' content={`Find recipes, create goals and notes, track stocks and view random pictures. Happy browsing!`} />
       <meta itemProp='image' content={`https://random-stuff-seven.vercel.app/images/logo-with-text-blue-small-social.png`} />
       {/* Twitter */}
+      <meta name='twitter:card' content='summary_large_image' />
       <meta property='twitter:title' content='Random Stuff' key='twitter-homeTitle' />
+      <meta property='twitter:domain' content='random-stuff-seven.vercel.app' />
+      <meta property='twitter:url' content='https://random-stuff-seven.vercel.app' />
+      <meta name='twitter:title' content='Random Stuff' />
       <meta property='twitter:description' content='Find recipes, create goals and notes, track stocks and view random pictures. Happy browsing!' />
-      <meta name='twitter:card' content='summary_large_image'></meta>
     </Header>
   )
 }
