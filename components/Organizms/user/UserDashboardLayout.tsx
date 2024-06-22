fa9d90257d57705b7719cd0f349b7c5098deace9
leo <leo_bel@hotmail.com>
2024-06-22 08:23:55 -0400

    maintenance

--- components/Organizms/user/UserDashboardLayout.tsx
+++ components/Organizms/user/UserDashboardLayout.tsx
@@ -2,6 +2,7 @@ import StockMarketGlance from '../stocks/StockMarketGlance'
 import NewsLayout from '../news/NewsLayout'
 import ScrollableBox from 'components/Atoms/Containers/ScrollableBox'
 import { useScrollTop } from 'components/Atoms/Boxes/useScrollTop'
+import { Box } from '@mui/material'
 
 const UserDashboardLayout = () => {
   const scroller = useScrollTop(0)
@@ -11,7 +12,9 @@ const UserDashboardLayout = () => {
       <ScrollableBox maxHeight={420} scroller={scroller}>
         <StockMarketGlance />
       </ScrollableBox>
-      <NewsLayout componentLoader />
+      <Box minHeight={400}>
+        <NewsLayout componentLoader />
+      </Box>
     </>
   )
 }
