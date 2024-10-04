2b9e0be55de60d978b50111e993bf73aff403630
leo <leo_bel@hotmail.com>
2024-10-04 11:53:27 -0400

    fixing profile fetch

--- pages/csr/stocks/details/[slug].tsx
+++ pages/csr/stocks/details/[slug].tsx
@@ -2,20 +2,36 @@ import ResponsiveContainer from 'components/Atoms/Boxes/ResponsiveContainer'
 import BackButton from 'components/Atoms/Buttons/BackButton'
 import Seo from 'components/Organizms/Seo'
 import StockDetailsLayout from 'components/Organizms/stocks/StockDetailsLayout'
+import { useUserController } from 'hooks/userController'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Page = () => {
   const router = useRouter()
+  const [loadingProfile, setLoadingProfile] = useState(true)
+  const { authProfile, setProfile, fetchProfilePassive } = useUserController()
 
   const id = router.query.slug as string
   const returnUrl = router.query['returnUrl'] as string | undefined
+
+  useEffect(() => {
+    const fn = async () => {
+      if (!authProfile) {
+        const p = await fetchProfilePassive()
+        setProfile(p)
+      }
+      setLoadingProfile(false)
+    }
+    fn()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authProfile])
+
   return (
     <>
       <Seo pageTitle={`Stock Details: ${id}`} />
       <ResponsiveContainer>
         <BackButton route={returnUrl} />
-        {id && <StockDetailsLayout symbol={id} disableCollapse />}
+        {id && !loadingProfile && <StockDetailsLayout symbol={id} disableCollapse />}
       </ResponsiveContainer>
     </>
   )
