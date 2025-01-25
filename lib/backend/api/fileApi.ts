18f6e8855c1797f9a029032ee36d7079ed4f5ac5
leo <leo_bel@hotmail.com>
2025-01-25 11:46:52 -0500

    removing logging

--- lib/backend/api/fileApi.ts
+++ lib/backend/api/fileApi.ts
@@ -2,9 +2,7 @@ import { createWorker } from 'tesseract.js'
 export async function getTextFromImage(url: string) {
   try {
     const worker = await createWorker('eng')
-    console.log(url)
     const ret = await worker.recognize(url, {})
-
     await worker.terminate()
     return ret.data
   } catch (err) {
