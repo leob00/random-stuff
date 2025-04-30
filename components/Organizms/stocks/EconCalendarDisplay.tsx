b012db0ce592d83980fb849eac0b1099f9c62686
leo <leo_bel@hotmail.com>
2025-04-30 06:22:33 -0400

    EcoCalendarExecuter fixes

--- components/Organizms/stocks/EconCalendarDisplay.tsx
+++ components/Organizms/stocks/EconCalendarDisplay.tsx
@@ -14,6 +14,7 @@ import { DropdownItem } from 'lib/models/dropdown'
 import StaticAutoComplete from 'components/Atoms/Inputs/StaticAutoComplete'
 import HorizontalDivider from 'components/Atoms/Dividers/HorizontalDivider'
 import { useLocalStore } from 'lib/backend/store/useLocalStore'
+import { sortArray } from 'lib/util/collections'
 
 dayjs.extend(isSameOrAfter)
 
@@ -44,7 +45,7 @@ const EconCalendarDisplay = ({
     }
   })
 
-  const countryDropdown = [{ text: 'All', value: 'all' }, ...countryOptions]
+  const countryDropdown = [{ text: 'All', value: 'all' }, ...sortArray(countryOptions, ['text'], ['asc'])]
 
   const filteredItems = filterItems(apiResult?.Items ?? [], econCalendarSettings?.filter)
 
