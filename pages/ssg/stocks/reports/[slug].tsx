c5101703a46ade1dcf02ba346603fb9f9ad06376
leo <leo_bel@hotmail.com>
2025-08-01 13:02:35 -0400

    maintenance

--- pages/ssg/stocks/reports/[slug].tsx
+++ pages/ssg/stocks/reports/[slug].tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import Seo from 'components/Organizms/Seo'
 import ResponsiveContainer from 'components/Atoms/Boxes/ResponsiveContainer'
 import PageHeader from 'components/Atoms/Containers/PageHeader'
-import { getReport } from 'lib/backend/api/qln/qlnApi'
+import { getReport, serverGetFetch } from 'lib/backend/api/qln/qlnApi'
 import { StockReportTypes } from 'lib/backend/api/qln/qlnModels'
 import { useSwrHelper } from 'hooks/useSwrHelper'
 import BackdropLoader from 'components/Atoms/Loaders/BackdropLoader'
@@ -33,9 +33,11 @@ const Page = () => {
     if (arg === 'indicesAndEtfs') {
       take = 2000
     }
+    const report = await serverGetFetch(`/StockReports?id=${arg}&take=100`)
+    let stocks = report.Body as StockQuote[]
     const result: Model = {
       type: arg,
-      items: await getReport(arg),
+      items: stocks, //await getReport(arg),
     }
 
     return result
