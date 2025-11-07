2f5110ad02934cc474edbdd3b420ace7ec651f0d
leo <leo_bel@hotmail.com>
2025-11-07 16:49:16 -0500

    ui upgrades

--- components/Atoms/Buttons/CloseIconButton.tsx
+++ components/Atoms/Buttons/CloseIconButton.tsx
@@ -1,14 +1,31 @@
-import Close from '@mui/icons-material/Close'
-import { Button, Chip, IconButton } from '@mui/material'
+//import Close from '@mui/icons-material/Close'
+import CancelIcon from '@mui/icons-material/Cancel'
+import { Button, IconButton, Typography, useTheme } from '@mui/material'
 
 const CloseIconButton = ({ onClicked }: { onClicked: () => void }) => {
   const handleClicked = () => {
     onClicked()
   }
+  const theme = useTheme()
   return (
-    <Button sx={{ borderRadius: '12px' }} onClick={handleClicked}>
-      <Chip label={'close'} variant='outlined' onDelete={handleClicked} color='primary' />
-    </Button>
+    <>
+      <Button
+        size='small'
+        variant='outlined'
+        sx={{ borderRadius: 140 }}
+        onClick={handleClicked}
+        endIcon={
+          <IconButton size='small' onClick={handleClicked} color='primary'>
+            <CancelIcon fontSize='small' />
+          </IconButton>
+        }
+      >
+        <Typography pl={1} variant='body2'>
+          close
+        </Typography>
+      </Button>
+      {/* <Chip label={'close'} variant='outlined' onDelete={handleClicked} color='primary' /> */}
+    </>
   )
 }
 
