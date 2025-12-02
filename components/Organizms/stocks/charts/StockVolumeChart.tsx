d77bb42ffe2c8ecc1852d7a6a38198d9f3b06c76
leo <leo_bel@hotmail.com>
2025-12-02 08:54:28 -0500

    stock volume chart added

--- components/Organizms/stocks/charts/StockVolumeChart.tsx
+++ components/Organizms/stocks/charts/StockVolumeChart.tsx
@@ -14,53 +14,50 @@ const StockVolumeChart = ({ data }: { data: StockHistoryItem[] }) => {
   const theme = useTheme()
   const { viewPortSize } = useViewPortSize()
 
-  const chartModel = useMemo(() => {
-    const isXSmallDevice = viewPortSize === 'xs'
-    let height = 200
-    switch (viewPortSize) {
-      case 'sm':
-        height = 300
-        break
-      case 'md':
-        height = 300
-        break
-      case 'lg':
-        height = 300
-        break
-      case 'xl':
-        height = 300
-        break
-    }
-    const chart: BarChart = {
-      labels: data.map((m) => m.TradeDate),
-      numbers: data.map((m) => m.Volume ?? 0),
-      colors: [CasinoBlue],
-    }
-    const options = getLineChartOptions(chart, 'volume', '', theme.palette.mode, true, false, isXSmallDevice)
-    options.scales!.y!.ticks = {
-      ...options.scales!.y!.ticks!,
-      callback: (value) => {
-        return `${numeral(value).format('0.00a')}`
-      },
-    }
-    options.plugins!.tooltip!.callbacks = {
-      ...options.plugins!.tooltip!.callbacks,
-      title: (tooltipItems) => {
-        return ` ${dayjs(tooltipItems[0].label).format('MM/DD/YYYY hh:mm a')}`
-      },
-      label: (tooltipItem) => {
-        return ` ${numeral(data[tooltipItem.dataIndex].Volume).format('0.00a')}`
-      },
-    }
+  const isXSmallDevice = viewPortSize === 'xs'
+  let height = 200
+  switch (viewPortSize) {
+    case 'sm':
+      height = 300
+      break
+    case 'md':
+      height = 300
+      break
+    case 'lg':
+      height = 300
+      break
+    case 'xl':
+      height = 300
+      break
+  }
+  const chart: BarChart = {
+    labels: data.map((m) => m.TradeDate),
+    numbers: data.map((m) => m.Volume ?? 0),
+    colors: [CasinoBlue],
+  }
+  const options = getLineChartOptions(chart, 'volume', '', theme.palette.mode, true, false, isXSmallDevice)
+  options.scales!.y!.ticks = {
+    ...options.scales!.y!.ticks!,
+    callback: (value) => {
+      return `${numeral(value).format('0.00a')}`
+    },
+  }
+  options.plugins!.tooltip!.callbacks = {
+    ...options.plugins!.tooltip!.callbacks,
+    title: (tooltipItems) => {
+      return ` ${dayjs(tooltipItems[0].label).format('MM/DD/YYYY hh:mm a')}`
+    },
+    label: (tooltipItem) => {
+      return ` ${numeral(data[tooltipItem.dataIndex].Volume).format('0.00a')}`
+    },
+  }
 
-    const result: TimeSeriesLineChartModel = {
-      chartData: chart,
-      chartOptions: options,
-      isXSmallDevice: isXSmallDevice,
-      height,
-    }
-    return result
-  }, [viewPortSize, data.length])
+  const chartModel: TimeSeriesLineChartModel = {
+    chartData: chart,
+    chartOptions: options,
+    isXSmallDevice: isXSmallDevice,
+    height,
+  }
 
   return (
     <Box>
